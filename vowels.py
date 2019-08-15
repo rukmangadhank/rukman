0a05e0fe3bbf2e143caa5636baf7e6ba40fd75d3
rukmangadhank <54092007+rukmangadhank@users.noreply.github.com>
2019-08-15 20:49:54 +0530

    Add files via upload

---
+++ vowels.py
@@ -0,0 +1,10 @@
+x = input("")
+
+if(x=='a' or x=='e' or x=='i' or x=='o' or x=='u' or x=='A' or x=='E' or x=='I' or x=='O' or x=='U'):
+    print("Vovel")
+
+elif(x=='b' or x=='c' or x=='d' or x=='f' or x=='g' or x=='h' or x=='j' or x=='k' or x=='l' or x=='m' or x=='n' or x=='p' or x=='q' or x=='r' or x=='s' or x=='t' or x=='v' or x=='w' or x=='x' or x=='y' or x=='z' ):
+     print("Consonant")
+    
+else:
+    print("Invalid")
