141e00bfc68cc52c307d33dc4845a31adea9d7e8
rukmangadhank <54092007+rukmangadhank@users.noreply.github.com>
2019-08-15 19:42:16 +0530

    Add files via upload

--- oddeven.py
+++ oddeven.py
@@ -1,9 +1,9 @@
 num= int(input(''))
-if num==-1:
-    print("invalid")
-a=num%2
-if a==0:
-    print("Even")
-if num==3:
- print("Odd")
 
+a=num%2
+if num<0:
+    print("invalid")
+elif a==0:
+    print("Even")
+else :
+ print("Odd")
